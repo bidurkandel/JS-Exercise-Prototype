a9f7ca73e0cb99804f5eb78c1d748f525a5625e8
bidurkandel <51921725+bidurkandel@users.noreply.github.com>
2021-01-27 13:19:14 -0800

    Done with JS EXERCISE PROTOTYPE

--- index.js
+++ index.js
@@ -39,16 +39,25 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
   }
- 
- 
 
-  
-  
-  
-  
+  Person.prototype.eat = function(food) {
+  if (this.stomach.length < 10) {
+  this.stomach.push(food);
+  };
+  }
+  Person.prototype.poop = function() {
+  this.stomach = [];
+  }
+
+  Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+  }
+
   /*
     TASK 2
       - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -63,9 +72,39 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
   }
+  Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+  }
+  Car.prototype.drive = function(distance) {
+  const gallonsConsumed = distance / this.milesPerGallon;
+  if (this.tank - gallonsConsumed >= 0) {
+  this.tank = Number((this.tank - gallonsConsumed).toFixed(2));
+  this.odometer += distance;
+  } else {
+  const extraFuelNeeded = Number((gallonsConsumed - this.tank).toFixed(2));
+  const actualDistance = Number((this.milesPerGallon * this.tank).toFixed(2));
+  this.tank = 0;
+  this.odometer += actualDistance;
+  console.log(`You ran out of fuel before completing your trip! You needed ${extraFuelNeeded} extra gallon${extraFuelNeeded === 1 ? "": "s"} to complete the trip. You only made it ${actualDistance} mile${actualDistance === 1 ? "": "s"}!`);
+  }
+  }
+  // Testing the fill and drive functionality
+  const mercedez = new Car('S5', 30);
+  console.log(mercedez);
+  mercedez.fill(5);
+  console.log(mercedez);
+  mercedez.drive(195);
+  console.log(mercedez);
+  mercedez.drive(145);
+  console.log(mercedez);
+  mercedez.fill(30);
+  console.log(mercedez);
   
   
   /*
@@ -75,18 +114,23 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
+  }
+  Baby.prototype = Object.create(Person.prototype);
+  Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
   }
  
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. It is used for window/global object binding. It is a pronoun to use in place of an object, gives you the object context. When in the global scope the value of "this" will be the window/console object. In the browser this is called winow an in node it's call global/console.
+    2. It is used for implicit Binding. Whenever a preceding dot calls a function, the object before the dot is "this"
+    3. It is used for new binding. In a constructor function, "this" refres to specific instance of the object that is created and returned by the constructor function.
+    4. it is used for explicit binding. we can override how we set values of constructor objects by taking the object oriented apporoach by calling constructors objects explicitly with new functions. when using dot call or dot apply method, this is explicitly defined.
   */
   
   
